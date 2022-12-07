8ea27e81d93547c3f8e4a303c10562e2ad5b536e
Elmo-S <elmo.salonen@gmail.com>
2022-12-07 22:53:52 +0000

    Added a library for the HSL API

---
+++ homeassistant/components/hsl/hsl_api.py
@@ -0,0 +1,30 @@
+"""Library for HSL API."""
+import aiohttp
+
+from .const import API_URL
+
+
+class HSL:
+    """Class for handling API calls and validation."""
+
+    def __init__(self, api_key, session: aiohttp.ClientSession) -> None:
+        """Initialize API instance."""
+        self.url = API_URL
+        self.header = {"content-type": "application/json"}
+        self.api_key = api_key
+        self._session = session
+
+    async def post(self, query: str):
+        """Make API call."""
+        response = await self._session.post(
+            self.url, json={"query": query}, headers=self.header
+        )
+        return response
+
+    async def authenticate(self) -> bool:
+        """Authenticate API connection."""
+        query = """{stop(id: "HSL:1140447") {name}}"""
+        response = await self.post(query)
+        if not response.status == 200:
+            return False
+        return True
